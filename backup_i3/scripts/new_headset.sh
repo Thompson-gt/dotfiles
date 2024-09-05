7d48bc308525305e8b30b952db7d2a990899ef3c
Thompson-gt <thompsongabriel440@gmail.com>
2024-09-05 18:46:00 -0400

    fixed the use of the wrong field in the headset script

--- backup_i3/scripts/new_headset.sh
+++ backup_i3/scripts/new_headset.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 HEADPHONES_MAC=$(bluetoothctl devices | awk '{print$2}')
-connected=$(bluetoothctl info $HEADPHONES_MAC | grep "Paired"| awk '{print$2}')
+ICON="🎧"
+
+
+connected=$(bluetoothctl info $HEADPHONES_MAC | grep "Connected"| awk '{print$2}')
 
 if [ *"${connected}"*  == *"yes"* ]; then {
     p=$(bluetoothctl info $HEADPHONES_MAC | grep "Battery Percentage" | awk '{print$4}')
-    echo $p"%"
+    echo $ICON$p"%"
 } 
 fi
 
