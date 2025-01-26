f20db9ea5bfc3a121f5fba1cc3256b7266a66c59
Thompson-gt <thompsongabriel440@gmail.com>
2025-01-25 19:03:48 -0500

    rewrite of lsp.lua moving away from lsp-zero, original config in lsp_zero.lua file

--- backup_nvim/lua/plugins/lsp.lua
+++ backup_nvim/lua/plugins/lsp.lua
@@ -1,146 +1,177 @@
 return {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    "williamboman/mason.nvim",
     dependencies = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {                                      -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-            pcall(vim.cmd, 'MasonUpdate')
-        end,
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "L3MON4D3/LuaSnip",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-nvim-lua",
     },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},     -- Required
-    {'hrsh7th/cmp-nvim-lsp'}, -- Required
-    {'L3MON4D3/LuaSnip'},     -- Required
-    {"hrsh7th/cmp-path"},
-    {"hrsh7th/cmp-cmdline"},
-    {"hrsh7th/cmp-buffer"},
-    {"hrsh7th/cmp-nvim-lua"},
-},
-config = function()
-    local lsp = require("lsp-zero")
-
-    lsp.preset("recommended")
-
-    lsp.ensure_installed({
-        'ts_ls',
-        'rust_analyzer',
-        'gopls'
-    })
-
-    -- Fix Undefined global 'vim'
-    lsp.nvim_workspace()
-    -- need all of this to change the keybinds for the default lsp????
-    local cmp = require('cmp')
-    local types = require("cmp.types")
+    config = function()
+        -- Add cmp_nvim_lsp capabilities settings to lspconfig
+        -- This should be executed before you configure any language server
+        local lspconfig_defaults = require('lspconfig').util.default_config
+        lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lspconfig_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+        )
 
 
-    local function deprioritize_snippet(entry1, entry2)
-        if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then return false end
-        if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then return true end
+
+        require('mason').setup({})
+        require('mason-lspconfig').setup({
+            ensure_installed = {
+                'ts_ls',
+                'rust_analyzer',
+                'gopls'
+            },
+            handlers = {
+                -- asigns default fucntionality to all lsp servers
+                function(server_name)
+                    require('lspconfig')[server_name].setup({})
+                end,
+                -- this will give lsp support to the neovim functions
+                lua_ls = function()
+                    require('lspconfig').lua_ls.setup({
+                        settings = {
+                            Lua = {
+                                runtime = {
+                                    version = 'LuaJIT',
+                                },
+                                diagnostics = {
+                                    globals = { 'vim' },
+                                },
+                                workspace = {
+                                    library = { vim.env.VIMRUNTIME },
+                                },
+                            },
+                        },
+                    })
+                end,
+                -- custom handler for lsps go here
+                -- ex: lsp_server = function `handling funciton` end
+            },
+        })
+
+
+        local cmp = require('cmp')
+        cmp.setup({
+            sources = {
+                { name = 'nvim_lsp' },
+                { name = 'cmp-nvim-lua' },
+                { name = 'hrsh7th/cmp-buffer' },
+                { name = 'hrsh7th/cmp-path' },
+            },
+
+            experimental = {
+                --ghost_text = true,
+            },
+            window = {
+                completion = cmp.config.window.bordered({
+                    winhighlight = "FloatBorder:DiagnosticSignOk,CursorLine:SpellRare"
+                }),
+                documentation = cmp.config.window.bordered({
+                    winhighlight = "FloatBorder:DiagnosticSignOk,CursorLine:SpellRare"
+                }),
+            },
+            mapping = cmp.mapping.preset.insert({
+                -- `control space` key to confirm completion
+                ['<C-Space>'] = cmp.mapping.confirm({ select = false }),
+
+                -- Ctrl+y to trigger completion menu
+                ['<C-y>'] = cmp.mapping.complete(),
+            }),
+        })
+
+        -- enable completion when typing commands
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({
+                { name = 'path' }
+            }, {
+                { name = 'cmdline' }
+            })
+        })
+
+        -- add aditional config to lsp 
+        --
+        --
+        vim.diagnostic.config {
+            underline = true,
+            italic = true,
+            virtual_text = true,
+            signs = true,
+            severity_sort = true,
+            update_in_insert = false,
+            border = true,
+
+        }
+
+        -- Add borders to floating windows
+        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+        vim.lsp.handlers.hover,
+        {border = 'single'}
+        )
+        vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+        vim.lsp.handlers.signature_help,
+        {border = 'single'}
+        )
+
+
+        -- removes the left shift when diagnostics start
+        vim.opt.signcolumn = 'yes'
+
+        vim.diagnostic.config({
+            signs = {
+                text = {
+                    [vim.diagnostic.severity.ERROR] = '✘',
+                    [vim.diagnostic.severity.WARN] = '▲',
+                    --[vim.diagnostic.severity.HINT] = '⚑',
+                    [vim.diagnostic.severity.INFO] = '»',
+                },
+            },
+        })
+
+
+
+
+        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+            vim.lsp.handlers.hover,
+            { border = 'rounded' }
+        )
+        vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+            vim.lsp.handlers.signature_help,
+            { border = 'rounded' }
+        )
+
+        vim.api.nvim_create_autocmd('LspAttach', {
+            callback = function(event)
+                local opts = { buffer = event.buf }
+                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+                vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
+                vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+                vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+                vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+                vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+                vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+                vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+                vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>',
+                    { noremap = true, silent = true })
+                vim.keymap.set({ 'n', 'x' }, '<leader>gf', function()
+                    vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+                end, opts)
+            end,
+        })
     end
 
-    cmp.setup({
-        sorting = {
-            priority_weight = 2,
-            comparators = {
-                deprioritize_snippet,
-                -- the rest of the comparators are pretty much the defaults
-                cmp.config.compare.offset,
-                cmp.config.compare.exact,
-                cmp.config.compare.scopes,
-                cmp.config.compare.score,
-                cmp.config.compare.recently_used,
-                cmp.config.compare.locality,
-                cmp.config.compare.kind,
-                cmp.config.compare.sort_text,
-                cmp.config.compare.length,
-                cmp.config.compare.order,
-            },
-        },
-        sources = {
-            {
-                name = 'nvim_lsp',
-                'cmp-nvim-lua',
-                "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-path"
-            },
-        },
-        experimental = {
-            --ghost_text = true,
-        },
-        window = {
-            completion = cmp.config.window.bordered({
-                winhighlight = "FloatBorder:DiagnosticSignOk,CursorLine:SpellRare"
-            }),
-            documentation = cmp.config.window.bordered({
-                winhighlight = "FloatBorder:DiagnosticSignOk,CursorLine:SpellRare"
-            }),
-        },
-        mapping = {
-            -- `control space` key to confirm completion
-            ['<C-Space>'] = cmp.mapping.confirm({select = false}),
-
-            -- Ctrl+y to trigger completion menu
-            ['<C-y>'] = cmp.mapping.complete(),
-        },
-    })
-
-
-    -- enable completion when typing commands
-    cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-            { name = 'path' }
-        }, {
-            { name = 'cmdline' }
-        })
-    })
-
-    lsp.set_preferences({
-        suggest_lsp_servers = false,
-        sign_icons = {
-            error = 'E',
-            warn = 'W',
-            hint = 'H',
-            info = 'I'
-        }
-    })
-
-    lsp.on_attach(function(_, bufnr)
-        local opts = {buffer = bufnr, remap = false}
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
-        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
-        vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
-        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
-        vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
-        vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
-    end)
-
-
-    lsp.setup()
-
-    vim.diagnostic.config {
-        underline = true,
-        italic = true,
-        virtual_text = true,
-        signs = true,
-        severity_sort = true,
-        update_in_insert = false,
-    }
-
-end
-    }
-
+}
---
+++ backup_nvim/lua/plugins/lsp_zero.lua
@@ -0,0 +1,147 @@
+return {}
+--return {
+--    'VonHeikemen/lsp-zero.nvim',
+--    branch = 'v2.x',
+--    dependencies = {
+--        -- LSP Support
+--        {'neovim/nvim-lspconfig'},             -- Required
+--        {                                      -- Optional
+--        'williamboman/mason.nvim',
+--        build = function()
+--            pcall(vim.cmd, 'MasonUpdate')
+--        end,
+--    },
+--    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+--
+--    -- Autocompletion
+--    {'hrsh7th/nvim-cmp'},     -- Required
+--    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+--    {'L3MON4D3/LuaSnip'},     -- Required
+--    {"hrsh7th/cmp-path"},
+--    {"hrsh7th/cmp-cmdline"},
+--    {"hrsh7th/cmp-buffer"},
+--    {"hrsh7th/cmp-nvim-lua"},
+--},
+--config = function()
+--    local lsp = require("lsp-zero")
+--
+--    lsp.preset("recommended")
+--
+--    lsp.ensure_installed({
+--        'ts_ls',
+--        'rust_analyzer',
+--        'gopls'
+--    })
+--
+--    -- Fix Undefined global 'vim'
+--    lsp.nvim_workspace()
+--    -- need all of this to change the keybinds for the default lsp????
+--    local cmp = require('cmp')
+--    local types = require("cmp.types")
+--
+--
+--    local function deprioritize_snippet(entry1, entry2)
+--        if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then return false end
+--        if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then return true end
+--    end
+--
+--    cmp.setup({
+--        sorting = {
+--            priority_weight = 2,
+--            comparators = {
+--                deprioritize_snippet,
+--                -- the rest of the comparators are pretty much the defaults
+--                cmp.config.compare.offset,
+--                cmp.config.compare.exact,
+--                cmp.config.compare.scopes,
+--                cmp.config.compare.score,
+--                cmp.config.compare.recently_used,
+--                cmp.config.compare.locality,
+--                cmp.config.compare.kind,
+--                cmp.config.compare.sort_text,
+--                cmp.config.compare.length,
+--                cmp.config.compare.order,
+--            },
+--        },
+--        sources = {
+--            {
+--                name = 'nvim_lsp',
+--                'cmp-nvim-lua',
+--                "hrsh7th/cmp-buffer",
+--                "hrsh7th/cmp-path"
+--            },
+--        },
+--        experimental = {
+--            --ghost_text = true,
+--        },
+--        window = {
+--            completion = cmp.config.window.bordered({
+--                winhighlight = "FloatBorder:DiagnosticSignOk,CursorLine:SpellRare"
+--            }),
+--            documentation = cmp.config.window.bordered({
+--                winhighlight = "FloatBorder:DiagnosticSignOk,CursorLine:SpellRare"
+--            }),
+--        },
+--        mapping = {
+--            -- `control space` key to confirm completion
+--            ['<C-Space>'] = cmp.mapping.confirm({select = false}),
+--
+--            -- Ctrl+y to trigger completion menu
+--            ['<C-y>'] = cmp.mapping.complete(),
+--        },
+--    })
+--
+--
+--    -- enable completion when typing commands
+--    cmp.setup.cmdline(':', {
+--        mapping = cmp.mapping.preset.cmdline(),
+--        sources = cmp.config.sources({
+--            { name = 'path' }
+--        }, {
+--            { name = 'cmdline' }
+--        })
+--    })
+--
+--    lsp.set_preferences({
+--        suggest_lsp_servers = false,
+--        sign_icons = {
+--            error = 'E',
+--            warn = 'W',
+--            hint = 'H',
+--            info = 'I'
+--        }
+--    })
+--
+--    lsp.on_attach(function(_, bufnr)
+--        local opts = {buffer = bufnr, remap = false}
+--        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+--        vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
+--        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
+--        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+--        vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
+--        vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end, opts)
+--        vim.keymap.set("n", "gs", function() vim.lsp.buf.signature_help() end, opts)
+--        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+--        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+--        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+--        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+--        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+--        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+--        vim.api.nvim_set_keymap('n', '<leader>dd', '<cmd>Telescope diagnostics<CR>', { noremap = true, silent = true })
+--    end)
+--
+--
+--    lsp.setup()
+--
+--    vim.diagnostic.config {
+--        underline = true,
+--        italic = true,
+--        virtual_text = true,
+--        signs = true,
+--        severity_sort = true,
+--        update_in_insert = false,
+--    }
+--
+--end
+--    }
+--
